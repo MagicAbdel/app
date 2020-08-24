e65de0a26ee2065ec146a49541ed498c264d8b3a
MagicAbdel <abdessalam.zaimi@epita.fr>
2020-08-24 22:36:43 +0200

    fixed regexp validator on fields

--- app/api/models.py
+++ app/api/models.py
@@ -1,25 +1,25 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator, RegexValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator
 from django.conf import settings
 from django.utils import timezone
 
 # Create your models here.
 class Parent(models.Model):
-    name = models.CharField(max_length=200, validators=[RegexValidator('[a-zA-Z]')])
-    lastname = models.CharField(max_length=200, validators=[RegexValidator('[a-zA-Z]')])
+    name = models.CharField(max_length=200, validators=[RegexValidator('^[a-zA-Z]+$')])
+    lastname = models.CharField(max_length=200, validators=[RegexValidator('^[a-zA-Z]+$')])
 
-    street = models.CharField(max_length=200, validators=[RegexValidator('[a-zA-Z0-9]')])
-    city = models.CharField(max_length=200, validators=[RegexValidator('[a-zA-Z]')])
-    zipcode = models.IntegerField(validators=[MaxLengthValidator(5), MinLengthValidator(5)])
-    state = models.CharField(max_length=200, validators=[RegexValidator('[a-zA-Z]')])
+    street = models.CharField(max_length=200, validators=[RegexValidator('^[a-zA-Z0-9]+$')])
+    city = models.CharField(max_length=200, validators=[RegexValidator('^[a-zA-Z]+$')])
+    zipcode = models.IntegerField(validators=[MinValueValidator(10000), MaxValueValidator(99999)])
+    state = models.CharField(max_length=200, validators=[RegexValidator('^[a-zA-Z]+$')])
 
     def __str__(self):
         return self.name + " " + self.lastname
 
 
 class Child(models.Model):
-    name = models.CharField(max_length=200, validators=[RegexValidator('[a-zA-Z]')])
-    lastname = models.CharField(max_length=200, validators=[RegexValidator('[a-zA-Z]')])
+    name = models.CharField(max_length=200, validators=[RegexValidator('^[a-zA-Z]+$')])
+    lastname = models.CharField(max_length=200, validators=[RegexValidator('^[a-zA-Z]+$')])
 
     parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
 
